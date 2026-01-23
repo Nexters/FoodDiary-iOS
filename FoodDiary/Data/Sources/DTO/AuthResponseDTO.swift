56c4fcc243b981f88e593ce68b20b22bf3b05efe
kanghun1121 <kanghun1121@naver.com>
2026-01-24 00:06:16 +0900

    feat: 로그인 관련 DTO, Entity 추가

---
+++ FoodDiary/Data/Sources/DTO/AuthResponseDTO.swift
@@ -0,0 +1,20 @@
+//
+//  AuthResponseDTO.swift
+//  Data
+//
+//  Created by 강대훈 on 1/23/26.
+//
+
+import Foundation
+
+struct AuthResponseDTO: Decodable {
+    let accessToken: String
+    let id: String
+    let isFirst: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case id
+        case isFirst = "is_first"
+    }
+}
