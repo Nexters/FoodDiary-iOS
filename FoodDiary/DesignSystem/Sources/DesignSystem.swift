87d91fa97386e08626da9bed56da1ef85afecc85
enebin <enebin777@gmail.com>
2026-01-25 19:55:37 +0900

    feat: MumukImage 에셋 접근 wrapper 추가

--- FoodDiary/DesignSystem/Sources/DesignSystem.swift
+++ FoodDiary/DesignSystem/Sources/DesignSystem.swift
@@ -5,8 +5,17 @@
 //  Created by 강대훈 on 1/12/26.
 //
 
-import Foundation
+import UIKit
 
+/// DesignSystem 모듈의 이미지 에셋 접근
+public enum MumukImage {
+    /// 체크마크 선택됨 이미지
+    public static var checkmarkSelected: UIImage? {
+        UIImage(named: "image_checked", in: Bundle.module, compatibleWith: nil)
+    }
 
-
-
+    /// 체크마크 미선택 이미지
+    public static var checkmarkUnselected: UIImage? {
+        UIImage(named: "image_unchecked", in: Bundle.module, compatibleWith: nil)
+    }
+}
