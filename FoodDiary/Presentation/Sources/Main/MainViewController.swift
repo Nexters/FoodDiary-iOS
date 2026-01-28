3d8c72a93a6b450dba14b44ca76db720dca05c71
kanghun1121 <kanghun1121@naver.com>
2026-01-29 01:09:04 +0900

    feat: MainViewController에 로그아웃 기능 추가
    MainViewController에서 토큰 삭제 및 로그아웃 기능을 구현했습니다.
    - TokenRepository를 생성자로 주입받도록 변경
    - 로그아웃 버튼 추가 및 deleteToken() 호출
    - didLogoutPublisher를 통해 로그아웃 이벤트 발행

--- FoodDiary/Presentation/Sources/Main/MainViewController.swift
+++ FoodDiary/Presentation/Sources/Main/MainViewController.swift
@@ -5,31 +5,69 @@
 //  Created by 강대훈 on 1/23/26.
 //
 
+import Combine
+import Domain
 import UIKit
 
 public final class MainViewController: UIViewController {
-    
+    private let tokenRepository: TokenRepository
+    private let didLogoutSubject = PassthroughSubject<Void, Never>()
+
+    public var didLogoutPublisher: AnyPublisher<Void, Never> {
+        didLogoutSubject.eraseToAnyPublisher()
+    }
+
+    public init(tokenRepository: TokenRepository) {
+        self.tokenRepository = tokenRepository
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     public override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
     }
-    
-    private func configureUI() {
+}
+
+private extension MainViewController {
+    func configureUI() {
         view.backgroundColor = .systemBackground
-        
+
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .white
         label.text = "메인화면"
-        
+
+        let logoutButton = UIButton(type: .system)
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        logoutButton.setTitle("로그아웃", for: .normal)
+        logoutButton.addTarget(self, action: #selector(logoutButtonTapped), for: .touchUpInside)
+
         view.addSubview(label)
-        
+        view.addSubview(logoutButton)
+
         NSLayoutConstraint.activate([
-            label.heightAnchor.constraint(equalToConstant: 50),
-            label.widthAnchor.constraint(equalToConstant: 50),
             label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+
+            logoutButton.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 24),
+            logoutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         ])
+    }
+
+    @objc
+    func logoutButtonTapped() {
+        do {
+            try tokenRepository.deleteToken()
+            didLogoutSubject.send()
+        } catch {
+            // TODO: 에러 처리 전략 확정 후 구체적인 UI 처리
+            print("토큰 삭제 실패: \(error)")
+        }
     }
 }
 
