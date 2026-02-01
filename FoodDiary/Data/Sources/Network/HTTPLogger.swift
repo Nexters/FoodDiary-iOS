4dcfb1ff18b92b7ec640e59b3a267bb9d502aa2e
kanghun1121 <kanghun1121@naver.com>
2026-02-02 00:03:13 +0900

    refactor: HTTPClient 로거 적용 및 에러 세분화

---
+++ FoodDiary/Data/Sources/Network/HTTPLogger.swift
@@ -0,0 +1,46 @@
+//
+//  HTTPLogger.swift
+//  Data
+//
+
+import Foundation
+import Logging
+
+public struct HTTPLogger {
+    private let logger: Logger
+
+    public init(logger: Logger = Logger(label: "com.fooddiary.network")) {
+        self.logger = logger
+    }
+
+    func logRequest(_ request: URLRequest) {
+        #if DEBUG
+        let method = request.httpMethod ?? "UNKNOWN"
+        let url = request.url?.absoluteString ?? "nil"
+        logger.info("[Request] [\(method)] \(url)")
+        #endif
+    }
+
+    func logResponse(_ response: URLResponse, statusCode: Int) {
+        #if DEBUG
+        let url = response.url?.absoluteString ?? "nil"
+        if (200..<300).contains(statusCode) {
+            logger.info("[Response] [\(statusCode)] \(url)")
+        } else {
+            logger.error("[Response] [\(statusCode)] \(url)")
+        }
+        #endif
+    }
+
+    func logDecodedModel<T>(_ model: T) {
+        #if DEBUG
+        logger.info("[Decoded] \(String(describing: type(of: model))): \(model)")
+        #endif
+    }
+
+    func logError(_ error: Error, context: String) {
+        #if DEBUG
+        logger.error("[Error] \(context): \(error.localizedDescription)")
+        #endif
+    }
+}
