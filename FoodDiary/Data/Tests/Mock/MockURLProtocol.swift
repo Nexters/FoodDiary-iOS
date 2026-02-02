146f125a30fbcd4d2355f6d9d171f98cafcfca60
KangKang <165460077+kanghun1121@users.noreply.github.com>
2026-02-02 17:32:36 +0900

    HTTPLogger 구현 & HTTPClient Test 작성 (#17)
    * feat: 로거 Dependency 추가
    
    * refactor: HTTPClient 로거 적용 및 에러 세분화
    
    * test: HTTPClient 테스트

---
+++ FoodDiary/Data/Tests/Mock/MockURLProtocol.swift
@@ -0,0 +1,47 @@
+//
+//  MockURLProtocol.swift
+//  Data
+//
+
+import Foundation
+
+final class MockURLProtocol: URLProtocol {
+    static var requestHandler: ((URLRequest) throws -> (URLResponse, Data))?
+
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else {
+            fatalError("MockURLProtocol.requestHandler is not set")
+        }
+
+        do {
+            let (response, data) = try handler(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+
+    override func stopLoading() {}
+}
+
+extension MockURLProtocol {
+    static func makeSession() -> URLSession {
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [MockURLProtocol.self]
+        return URLSession(configuration: configuration)
+    }
+
+    static func reset() {
+        requestHandler = nil
+    }
+}
