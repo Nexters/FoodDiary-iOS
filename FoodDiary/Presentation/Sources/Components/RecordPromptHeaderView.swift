1a898fbc2b71561e504dfe43d2ef79844b95bee9
kanghun1121 <kanghun1121@naver.com>
2026-02-10 12:13:05 +0900

    refactor: RecordPromptHeaderView 이름을 주입받을 수 있도록 개선

--- FoodDiary/Presentation/Sources/Components/RecordPromptHeaderView.swift
+++ FoodDiary/Presentation/Sources/Components/RecordPromptHeaderView.swift
@@ -7,10 +7,11 @@ import UIKit
 import DesignSystem
 
 /// 음식 기록 안내 헤더 뷰
-///
-/// "이번주 음식을 기록해 보세요" 서브타이틀과
-/// "길동님의 음식 기록\n지금 바로 쓸 수 있어요" 타이틀을 포함하는 Vertical StackView 컴포넌트
 public final class RecordPromptHeaderView: UIStackView {
+    // MARK: - Properties
+
+    private let userName: String
+
     // MARK: - UI Components
 
     private let subtitleLabel: UILabel = {
@@ -19,16 +20,17 @@ public final class RecordPromptHeaderView: UIStackView {
         return label
     }()
 
-    private let titleLabel: UILabel = {
+    private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
-        label.setText("길동님의 음식 기록\n지금 바로 쓸 수 있어요", style: .hd20, color: DesignSystemAsset.gray050.color)
+        label.setText("\(userName)님의 음식 기록\n지금 바로 쓸 수 있어요", style: .hd20, color: DesignSystemAsset.gray050.color)
         return label
     }()
 
     // MARK: - Initialization
 
-    public init() {
+    public init(userName: String = "길동") {
+        self.userName = userName
         super.init(frame: .zero)
         setupUI()
     }
