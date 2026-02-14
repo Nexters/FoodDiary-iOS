759052755bd099a2e632cac18ce298d509d976a3
kanghun1121 <kanghun1121@naver.com>
2026-02-14 22:43:29 +0900

    feat: RootTabBarController 및 CalendarViewController 구현
    - 홈, 주간, 월간, 인사이트 4개 탭 구성
    - 각 탭별 ViewController 연결
    - 탭바 스타일 및 선택 상태 처리

---
+++ FoodDiary/Presentation/Sources/Calendar/CalendarViewController.swift
@@ -0,0 +1,64 @@
+//
+//  CalendarViewController.swift
+//  Presentation
+//
+//  Created by 강대훈 on 2/13/26.
+//
+
+import UIKit
+import Combine
+
+public final class CalendarViewController: UIViewController {
+    public enum ViewMode {
+        case weekly
+        case monthly
+    }
+
+    private let weeklyVC: UIViewController
+    private let monthlyVC: UIViewController
+    private let currentModeSubject = CurrentValueSubject<ViewMode, Never>(.weekly)
+    private var currentChild: UIViewController?
+
+    public var currentModePublisher: AnyPublisher<ViewMode, Never> {
+        currentModeSubject.eraseToAnyPublisher()
+    }
+
+    public init(weeklyVC: UIViewController, monthlyVC: UIViewController) {
+        self.weeklyVC = weeklyVC
+        self.monthlyVC = monthlyVC
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        showViewController(for: currentModeSubject.value)
+    }
+
+    public func toggleViewMode() {
+        let newMode: ViewMode = currentModeSubject.value == .weekly ? .monthly : .weekly
+        currentModeSubject.send(newMode)
+        showViewController(for: newMode)
+    }
+
+    private func showViewController(for mode: ViewMode) {
+        let targetVC = mode == .weekly ? weeklyVC : monthlyVC
+
+        if let currentChild {
+            currentChild.willMove(toParent: nil)
+            currentChild.view.removeFromSuperview()
+            currentChild.removeFromParent()
+        }
+        
+        addChild(targetVC)
+        view.addSubview(targetVC.view)
+        targetVC.view.frame = view.bounds
+        targetVC.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        targetVC.didMove(toParent: self)
+
+        currentChild = targetVC
+    }
+}
---
+++ FoodDiary/Presentation/Sources/TabBar/RootTabBarController.swift
@@ -0,0 +1,79 @@
+//
+//  RootTabBarController.swift
+//  Presentation
+//
+//  Created by 강대훈 on 2/13/26.
+//
+
+import UIKit
+import DesignSystem
+import Combine
+
+public final class RootTabBarController: UITabBarController {
+    let calendarVC: CalendarViewController
+    let insightVC: UIViewController
+    private var cancellables = Set<AnyCancellable>()
+
+    public init(calendarVC: CalendarViewController, insightVC: UIViewController) {
+        self.calendarVC = calendarVC
+        self.insightVC = insightVC
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() {
+        self.delegate = self
+
+        calendarVC.currentModePublisher
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] mode in
+                self?.updateToggleIcon(for: mode)
+            }
+            .store(in: &cancellables)
+
+        let calendarNav = UINavigationController(rootViewController: calendarVC)
+        calendarNav.tabBarItem = UITabBarItem(title: "홈", image: DesignSystemAsset.iconHome.image, tag: 0)
+
+        let insightNav = UINavigationController(rootViewController: insightVC)
+        insightNav.tabBarItem = UITabBarItem(title: "인사이트", image: DesignSystemAsset.iconInsight.image, tag: 1)
+
+        let toggleVC = UIViewController()
+        let toggleNav = UINavigationController(rootViewController: toggleVC)
+        toggleNav.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 2)
+        toggleNav.tabBarItem.image = DesignSystemAsset.iconWeekly.image
+
+        viewControllers = [calendarNav, insightNav, toggleNav]
+    }
+
+    private func toggleViewMode() {
+        calendarVC.toggleViewMode()
+    }
+
+    private func updateToggleIcon(for mode: CalendarViewController.ViewMode) {
+        guard let items = tabBar.items, items.count > 2 else { return }
+
+        let newImage = mode == .monthly ? DesignSystemAsset.iconWeekly.image : DesignSystemAsset.iconMonthly.image
+        items[2].image = newImage
+    }
+}
+
+extension RootTabBarController: UITabBarControllerDelegate {
+    public func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        guard let index = viewControllers?.firstIndex(of: viewController) else { return true }
+
+        if index == 2 {
+            toggleViewMode()
+            return false
+        }
+
+        return true
+    }
+}
