1e0c9fd89bb65fcc2510589a856497e80781feed
kanghun1121 <kanghun1121@naver.com>
2026-02-15 19:49:28 +0900

    chore: DI 컨테이너 설정 업데이트
    새로 추가된 Device/Push/Notification 기능을 DI 컨테이너에 등록
    - DeviceInfoProvider, PushTokenStorage, NotificationAuthorizationProvider 등록
    - FinalizeAppleLoginUseCase에 새 의존성 주입
    - TokenManager → AuthTokenStorage 변경 반영

--- FoodDiary/App/Sources/AppDelegate.swift
+++ FoodDiary/App/Sources/AppDelegate.swift
@@ -15,7 +15,7 @@ import DI
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         Messaging.messaging().delegate = self
