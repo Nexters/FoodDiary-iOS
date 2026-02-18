99614fd07ca52dc6409b5027b76dda320ef4a9ca
kanghun1121 <kanghun1121@naver.com>
2026-02-18 23:04:26 +0900

    feat: FoodRecordRepositoryImpl 구현

---
+++ FoodDiary/Data/Sources/Repository/FoodRecordRepository.swift
@@ -0,0 +1,35 @@
+//
+//  FoodRecordRepository.swift
+//  Data
+//
+//  Created by 강대훈 on 2/18/26.
+//
+
+import Domain
+import Foundation
+
+public struct FoodRecordRepositoryImpl<Client: HTTPClienting & Sendable, Storage: AuthTokenStoring & Sendable>: FoodRecordRepository {
+    private let httpClient: Client
+    private let tokenStorage: Storage
+
+    public init(httpClient: Client, tokenStorage: Storage) {
+        self.httpClient = httpClient
+        self.tokenStorage = tokenStorage
+    }
+
+    public func fetchRecords(in dateRange: ClosedRange<Date>) async throws -> [Date: [FoodRecord]] {
+        let endpoint = DiaryEndpoint.fetchMonthlyTest(startDate: dateRange.lowerBound.apiDateString, endDate: dateRange.upperBound.apiDateString)
+        let response: MonthlyDiaryResponseDTO = try await httpClient.request(endpoint, accessToken: tokenStorage.get())
+        return response.toDomain()
+    }
+
+    public func fetchRecords(for date: Date) async throws -> [FoodRecord] {
+        // TODO: 구현 예정
+        return []
+    }
+
+    public func uploadRecord(_ request: CreateFoodRecordRequest) async throws -> String {
+        // TODO: 구현 예정
+        return ""
+    }
+}
