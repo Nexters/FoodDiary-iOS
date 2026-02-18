16db1ff8469594e61236b2f893c9034b295a0a1a
enebin <enebin777@gmail.com>
2026-02-18 19:39:29 +0900

    refactor: `AddressSearchResult` 엔티티로 분리

--- FoodDiary/Domain/Sources/Repository/AddressSearchRepository.swift
+++ FoodDiary/Domain/Sources/Repository/AddressSearchRepository.swift
@@ -5,17 +5,6 @@
 
 import Foundation
 
-/// 주소 검색 결과
-public struct AddressSearchResult: Equatable, Sendable {
-    public let placeName: String
-    public let roadAddress: String
-
-    public init(placeName: String, roadAddress: String) {
-        self.placeName = placeName
-        self.roadAddress = roadAddress
-    }
-}
-
 /// 주소 검색 Repository 프로토콜
 public protocol AddressSearchRepository: Sendable {
     /// 키워드로 주소 검색
