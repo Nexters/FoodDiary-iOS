78bccb5ea839f71ad8490c98f297094619fde8ba
kanghun1121 <kanghun1121@naver.com>
2026-02-19 22:24:02 +0900

    chore: 필요없는 코드 삭제

--- FoodDiary/Data/Sources/Repository/FoodRecordRepository.swift
+++ FoodDiary/Data/Sources/Repository/FoodRecordRepository.swift
@@ -30,7 +30,6 @@ public struct FoodRecordRepositoryImpl<Client: HTTPClienting & Sendable, Storage
 
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
-        formatter.locale = Locale(identifier: "en_US_POSIX")
 
         return response.reduce(into: [Date: [URL]]()) { result, entry in
             guard let date = formatter.date(from: entry.key) else { return }
