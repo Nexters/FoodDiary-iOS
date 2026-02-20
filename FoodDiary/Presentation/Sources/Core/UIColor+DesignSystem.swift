bee92fcd4cc7dcd2d0990585d55781afdf33cedd
kanghun1121 <kanghun1121@naver.com>
2026-02-21 01:31:47 +0900

    feat: 마이페이지 DesignSystem 에셋 추가
    - sd700 색상 추가
    - 아이콘 에셋 추가 (alert, arrow, logout, setting)
    - icon-next 이미지 업데이트
    - UIColor extension에 sd700 추가

--- FoodDiary/Presentation/Sources/Core/UIColor+DesignSystem.swift
+++ FoodDiary/Presentation/Sources/Core/UIColor+DesignSystem.swift
@@ -32,6 +32,7 @@ public extension UIColor {
     static let sdBase = DesignSystemAsset.sdBase.color
     static let sd900 = DesignSystemAsset.sd900.color
     static let sd800 = DesignSystemAsset.sd800.color
+    static let sd700 = DesignSystemAsset.sd700.color
     static let sd600 = DesignSystemAsset.sd600.color
 
     // MARK: - Grayscale
