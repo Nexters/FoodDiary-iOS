083c96a9755a1bf96f534a9f9b6b24ab406658ea
kanghun1121 <kanghun1121@naver.com>
2026-02-21 01:32:30 +0900

    feat: 마이페이지 UI 컴포넌트 추가
    - ProfileHeaderView 추가 (프로필 헤더)
    - WithdrawalFooterView 추가 (탈퇴 버튼 푸터)

---
+++ FoodDiary/Presentation/Sources/MyPage/Components/WithdrawalFooterView.swift
@@ -0,0 +1,33 @@
+//
+//  WithdrawalFooterView.swift
+//  Presentation
+//
+//  Created by 강대훈 on 2/21/26.
+//
+
+import UIKit
+
+final class WithdrawalFooterView: UIView {
+
+    private let withdrawalLabel: UILabel = {
+        let label = UILabel()
+        label.setText("탈퇴", style: .p12, color: .gray050)
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .clear
+        addSubview(withdrawalLabel)
+
+        withdrawalLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.centerY.equalToSuperview()
+        }
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
