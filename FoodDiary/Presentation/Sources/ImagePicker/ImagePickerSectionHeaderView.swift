2bb86c2fa9602fec26c16b818545ab535e289100
enebin <enebin777@gmail.com>
2026-02-22 13:03:29 +0900

    fix: 피커 잔버그 수정

--- FoodDiary/Presentation/Sources/ImagePicker/ImagePickerSectionHeaderView.swift
+++ FoodDiary/Presentation/Sources/ImagePicker/ImagePickerSectionHeaderView.swift
@@ -11,14 +11,23 @@ final class ImagePickerSectionHeaderView: UICollectionReusableView {
 
     static let reuseIdentifier = "ImagePickerSectionHeaderView"
 
+    private let guideLabel = UILabel()
     private let titleLabel = UILabel()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+
+        addSubview(guideLabel)
         addSubview(titleLabel)
+
+        guideLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(16)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+
         titleLabel.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(16)
-            $0.centerY.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(8)
+            $0.leading.trailing.equalToSuperview().inset(16)
         }
     }
 
@@ -27,7 +36,14 @@ final class ImagePickerSectionHeaderView: UICollectionReusableView {
         fatalError("init(coder:) has not been implemented")
     }
 
-    func configure(title: String) {
-        titleLabel.setText(title, style: .hd18, color: .white)
+    func configure(title: String, guideText: String? = nil) {
+        titleLabel.setText(title, style: .hd16, color: .white)
+
+        if let guideText {
+            guideLabel.setText(guideText, style: .p14, color: .gray400)
+            guideLabel.isHidden = false
+        } else {
+            guideLabel.isHidden = true
+        }
     }
 }
