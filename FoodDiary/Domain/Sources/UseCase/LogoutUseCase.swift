e8ef2cd7adabf018cf602074259e557c08e96935
kanghun1121 <kanghun1121@naver.com>
2026-04-11 16:56:43 +0900

    refactor: LoginSession 기반으로 로그아웃/탈퇴 이벤트 전달 통합

--- FoodDiary/Domain/Sources/UseCase/LogoutUseCase.swift
+++ FoodDiary/Domain/Sources/UseCase/LogoutUseCase.swift
@@ -9,15 +9,19 @@ import Foundation
 
 public struct LogoutUseCase {
     private let authRepository: AuthRepository
+    private let loginSession: LoginSession
 
-    public init(authRepository: AuthRepository) {
+    public init(authRepository: AuthRepository, loginSession: LoginSession) {
         self.authRepository = authRepository
+        self.loginSession = loginSession
     }
 
     /// 로그아웃을 수행합니다.
     ///
-    /// AuthTokenStorage를 clear하여 저장된 액세스 토큰을 제거합니다.
+    /// AuthTokenStorage를 clear하여 저장된 액세스 토큰을 제거하고,
+    /// LoginSession을 통해 로그아웃 이벤트를 전달합니다.
     public func execute() throws {
         try authRepository.logout()
+        loginSession.notifyLogout()
     }
 }
