6884b52446259dc9788257f8441449804f1c6f33
kanghun1121 <kanghun1121@naver.com>
2026-04-27 19:17:22 +0900

    refactor: iOS 최소버전 수정

--- FoodDiary/DesignSystem/Project.swift
+++ FoodDiary/DesignSystem/Project.swift
@@ -15,7 +15,7 @@ let project = Project(
             destinations: [.iPhone],
             product: .framework,
             bundleId: "com.fooddiary.designsystem",
-            deploymentTargets: .iOS("18.0"),
+            deploymentTargets: .iOS("26.0"),
             sources: ["Sources/**"],
             resources: ["Resources/**"],
             dependencies: [
